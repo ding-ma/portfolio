d4be6ad94b22c300fde371f62616926b07408cd1
ding-ma <ding.ma@mail.mcgill.ca>
2020-08-27 14:35:52 -0400

    fixing cors error

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,22 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(functions.config().sendgrid.key);
 
 
 exports.contactMe = functions.https.onRequest((request, response) => {
-  if(request.method !== "POST"){
-    response.status(400).send('Please send a POST request');
-    return;
+  if (request.method !== "POST") {
+    return response.status(400).send('Please send a POST request');
   }
-  let data = request.body;
-  const msg = {
-    to: 'ma.ding.dm@gmail.com',
-    from: 'contact.me@portfolio-ding.firebaseapp.com',
-    subject: data["subject"],
-    text: data["msg"],
-  };
-  sgMail.send(msg);
-  response.status(200).send('Email sent!')
+  return cors(request, response, () => {
+    let data = request.body;
+    const msg = {
+      to: 'ma.ding.dm@gmail.com',
+      from: 'contact.me@portfolio-ding.firebaseapp.com',
+      subject: data["subject"],
+      text: data["msg"],
+    };
+    sgMail.send(msg);
+    return response.status(200).send('Email sent!')
+  })
 });
