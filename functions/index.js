e3c1eef7901ac7232e007643fb99597de356d249
ding-ma <ding.ma@mail.mcgill.ca>
2020-08-27 10:35:36 -0400

    testing out sendgrid

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from github CI!");
+  sgMail.setApiKey(functions.config().sendgrid.key);
+  const msg = {
+    to: 'ma.ding.dm@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  response.send("Email sent");
 });
